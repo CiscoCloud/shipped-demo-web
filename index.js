47301428e5396e06ca3d140eb89872e6525de095
nehayward <nehayward@gmail.com>
2016-04-19 16:19:33 -0500

    Fixed env names

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var serveStatic = require("serve-static");
 
@@ -6,21 +5,17 @@ var app = express();
 app.use(serveStatic(__dirname + "/."));
 
 
-app.get('/endpoints', function (req, res) {
-	var eps={}
-	eps.Account 	= process.env.ACCOUNT_HOST
-	eps.Catalog		= process.env.CATALOG_HOST
-	eps.Cart		= process.env.CART_HOST
+app.get('/endpoints', function(req, res) {
+    var eps = {}
+    eps.Account = process.env.SHIPPED_DEMO_ACCOUNT
+    eps.Catalog = process.env.SHIPPED_DEMO_CATALOG
+    eps.Cart = process.env.SHIPPED_DEMO_CART
 
-	// eps.Account= "http://staging--shop--account--a6cc3a.shipped-cisco.com"
-	// eps.Catalog		=  "http://staging--shop--catalog--dba7f3.shipped-cisco.com"
-  // eps.Cart		="http://staging--shop--cart--26a94c.shipped-cisco.com"
+    // eps.Account= "http://localhost:8887"
+    // eps.Catalog		=  "http://localhost:8889"
+    // eps.Cart		="http://localhost:8888"
 
-	// eps.Account= "http://localhost:8887"
-	// eps.Catalog		=  "http://localhost:8889"
-  // eps.Cart		="http://localhost:8888"
-
-	res.json(eps);
+    res.json(eps);
 });
 
 //Server listening port.
